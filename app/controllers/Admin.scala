5247d5c8e79127ecc40a668f2d3b486132546334
Michael Skogberg <malliina123@gmail.com>
2017-01-15 16:12:21 +0100

    Fix oauth; fix proxies

--- app/controllers/Admin.scala
+++ app/controllers/Admin.scala
@@ -2,10 +2,14 @@ package controllers
 
 import akka.stream.Materializer
 import com.malliina.play.controllers.OAuthControl
-import play.api.mvc.Call
+import play.api.mvc.{Call, RequestHeader}
 
 class Admin(val mat: Materializer) extends OAuthControl(mat) {
   override def isAuthorized(email: String): Boolean = email == "malliina123@gmail.com"
+
+  // temp hack
+  override def redirURL(request: RequestHeader): String =
+    oAuthRedir.absoluteURL(secure = true)(request)
 
   override def startOAuth: Call = routes.Admin.initiate()
 
