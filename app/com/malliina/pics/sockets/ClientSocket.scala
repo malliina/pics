521e4512b7fa2ca20bd73d7ecd632b0e41fa6691
Michael Skogberg <michael.skogberg@futurice.com>
2018-01-17 16:08:35 +0100

    Change logger

--- app/com/malliina/pics/sockets/ClientSocket.scala
+++ app/com/malliina/pics/sockets/ClientSocket.scala
@@ -1,16 +1,20 @@
 package com.malliina.pics.sockets
 
-import akka.actor.{ActorLogging, ActorRef, Props}
+import akka.actor.{ActorRef, Props}
+import com.malliina.pics.sockets.ClientSocket.log
 import com.malliina.pics.sockets.ClientSockets._
 import com.malliina.pics.{PicRequest, ProfileInfo}
 import com.malliina.play.ws.{ActorMeta, JsonActor}
+import play.api.Logger
 import play.api.libs.json.Json
 
 object ClientSocket {
+  private val log = Logger(getClass)
+
   def props(ctx: SocketContext) = Props(new ClientSocket(ctx))
 }
 
-class ClientSocket(ctx: SocketContext) extends JsonActor(ctx) with ActorLogging {
+class ClientSocket(ctx: SocketContext) extends JsonActor(ctx) {
   override def preStart(): Unit = {
     super.preStart()
     ctx.mediator ! ClientJoined(PicClient(ctx.out, ctx.user))
