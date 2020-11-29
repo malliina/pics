3782a8f414dd840f92bfb5576141a4cc9a63bbe9
Michael Skogberg <malliina123@gmail.com>
2020-11-29 20:21:16 +0200

    Add header-based auth for JSON clients

--- backend/src/main/scala/com/malliina/pics/auth/Http4sAuth.scala
+++ backend/src/main/scala/com/malliina/pics/auth/Http4sAuth.scala
@@ -1,22 +1,29 @@
 package com.malliina.pics.auth
 
-import cats.effect.IO
-import com.malliina.pics.http4s.Reverse
-import com.malliina.pics.{AppConf, PicRequest2}
+import cats.effect.{ContextShift, IO}
+import com.malliina.http.OkClient
+import com.malliina.pics.http4s.PicsImplicits._
+import com.malliina.pics.http4s.PicsService.{noCache, version10}
+import com.malliina.pics.http4s.{PicsService, Reverse}
+import com.malliina.pics.{AppConf, Errors, PicRequest2}
+import com.malliina.play.auth.{CognitoAccessValidator, CognitoIdValidator, JWTUser, Validators}
 import com.malliina.values.{AccessToken, IdToken, Username}
-import org.http4s.dsl.io._
-import org.http4s.headers.{Cookie, Location}
-import org.http4s.{Headers, Response, ResponseCookie}
+import org.http4s.Credentials.Token
+import org.http4s.headers.{Authorization, Cookie, Location}
+import org.http4s.{Headers, MediaType, Response, ResponseCookie}
 import play.api.libs.json.{Json, OWrites, Reads, Writes}
 
 import scala.concurrent.duration.DurationInt
 
 object Http4sAuth {
-  def apply(conf: AppConf): Http4sAuth =
+  def apply(conf: AppConf, cs: ContextShift[IO]): Http4sAuth =
     new Http4sAuth(
       JWT(conf.secret),
+      Validators.picsAccess,
+      Validators.picsId,
+      Validators.google(OkClient.default),
       CookieConf("picsUser", "picsState", "picsReturnUri", "picsLastId")
-    )
+    )(cs)
 
   case class TwitterState(requestToken: AccessToken)
 
@@ -25,12 +32,35 @@ object Http4sAuth {
   }
 }
 
-class Http4sAuth(val jwt: JWT, conf: CookieConf) {
+// Contextshift required as long as we use Future-based GoogleTokenAuth.validate
+class Http4sAuth(
+  val webJwt: JWT,
+  ios: CognitoAccessValidator,
+  android: CognitoIdValidator,
+  google: GoogleTokenAuth,
+  conf: CookieConf
+)(implicit cs: ContextShift[IO]) {
   val returnUriKey = conf.returnUriKey
   val lastIdKey = conf.lastIdKey
   val cookiePath = Option("/")
 
-  def web(headers: Headers): Either[IO[Response[IO]], PicRequest2] = user(headers).fold(
+  def authenticate(headers: Headers): IO[Either[IO[Response[IO]], PicRequest2]] = {
+    val rs = PicsService.ranges(headers)
+    if (rs.exists(_.satisfies(MediaType.text.html))) {
+      IO.pure(web(headers))
+    } else if (rs.exists(m => m.satisfies(version10) || m.satisfies(MediaType.application.json))) {
+      readJwt(headers).map { e =>
+        e.fold(
+          err => Left(onUnauthorized(headers)),
+          user => Right(PicRequest2.forUser(user.username, headers))
+        )
+      }
+    } else {
+      IO.pure(Left(NotAcceptable(Json.toJson(Errors.single("Not acceptable.")), noCache)))
+    }
+  }
+
+  private def web(headers: Headers): Either[IO[Response[IO]], PicRequest2] = user(headers).fold(
     {
       case MissingCredentials2(_, headers) => Right(PicRequest2.anon(headers))
       case _                               => Left(onUnauthorized(headers))
@@ -38,39 +68,69 @@ class Http4sAuth(val jwt: JWT, conf: CookieConf) {
     user => Right(PicRequest2.forUser(user, headers))
   )
 
+  def readJwt(headers: Headers): IO[Either[IdentityError2, JWTUser]] = token(headers)
+    .map { token =>
+      IO(ios.validate(AccessToken(token.value)).orElse(android.validate(token)))
+        .flatMap { e =>
+          e.fold(
+            err =>
+              IO.fromFuture(IO(google.validate(token))).map { e =>
+                e.map { email => EmailUser(email) }
+              },
+            user => IO.pure(Right(user))
+          )
+        }
+        .map { e =>
+          e.left.map { error =>
+            JWTError2(error, headers)
+          }
+        }
+    }
+    .fold(e => IO.pure(Left(e)), identity)
+
+  def token(headers: Headers) = headers
+    .get(Authorization)
+    .toRight(MissingCredentials2("Missing Authorization header", headers))
+    .flatMap(_.credentials match {
+      case Token(_, token) => Right(IdToken(token))
+      case _               => Left(MissingCredentials2("Missing token.", headers))
+    })
+
   def session[T: Reads](from: Headers): Either[IdentityError2, T] = read[T](conf.sessionKey, from)
+
   def withSession[T: OWrites](t: T, res: Response[IO]): res.Self =
     withJwt(conf.sessionKey, t, res)
+
   def clearSession(res: Response[IO]): res.Self =
     res
       .removeCookie(ResponseCookie(conf.sessionKey, "", path = cookiePath))
       .removeCookie(ResponseCookie(conf.userKey, "", path = cookiePath))
 
-  def user(headers: Headers): Either[IdentityError2, Username] =
+  private def user(headers: Headers): Either[IdentityError2, Username] =
     readUser(conf.userKey, headers)
 
   def withUser[T: Writes](t: T, res: Response[IO]): res.Self =
     withJwt(conf.userKey, t, res)
 
   def withJwt[T: Writes](cookieName: String, t: T, res: Response[IO]): res.Self = {
-    val signed = jwt.sign(t, 12.hours)
+    val signed = webJwt.sign(t, 12.hours)
     res.addCookie(ResponseCookie(cookieName, signed.value, httpOnly = true, path = cookiePath))
   }
 
   private def readUser(cookieName: String, headers: Headers): Either[IdentityError2, Username] =
     read[UserPayload](cookieName, headers).map(_.username)
 
-  def read[T: Reads](cookieName: String, headers: Headers): Either[IdentityError2, T] =
+  private def read[T: Reads](cookieName: String, headers: Headers): Either[IdentityError2, T] =
     for {
       header <- Cookie.from(headers).toRight(MissingCredentials2("Cookie parsing error.", headers))
       cookie <- header.values
         .find(_.name == cookieName)
         .map(c => IdToken(c.content))
         .toRight(MissingCredentials2(s"Cookie not found: '$cookieName'.", headers))
-      t <- jwt.verify[T](cookie).left.map { err =>
+      t <- webJwt.verify[T](cookie).left.map { err =>
         JWTError2(err, headers)
       }
     } yield t
 
-  def onUnauthorized(headers: Headers) = SeeOther(Location(Reverse.signIn))
+  private def onUnauthorized(headers: Headers) = SeeOther(Location(Reverse.signIn))
 }
