cb766d05a4db025e1fe0ca933822c8522e472b55
Michael Skogberg <malliina123@gmail.com>
2022-01-06 20:27:59 +0200

    Inline small files

--- frontend/webpack.base.config.js
+++ frontend/webpack.base.config.js
@@ -38,7 +38,7 @@ const WebApp = merge(ScalaJS, {
       },
       {
         test: /logo-.*\.(png|svg)$/,
-        type: 'asset/resource',
+        type: 'asset',
         generator: {
           filename: 'static/img/[name]-[hash][ext]'
         }
