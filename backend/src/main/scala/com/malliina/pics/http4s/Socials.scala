930bffaa0edffb789582f2d9d3f004e8b50f24b7
Michael Skogberg <michael.skogberg@futurice.com>
2023-05-28 15:18:06 +0300

    Enable siwa

--- backend/src/main/scala/com/malliina/pics/http4s/Socials.scala
+++ backend/src/main/scala/com/malliina/pics/http4s/Socials.scala
@@ -1,14 +1,13 @@
 package com.malliina.pics.http4s
 
-import Socials.log
 import cats.effect.{IO, Sync}
 import com.malliina.http.HttpClient
 import com.malliina.pics.auth.*
-import com.malliina.pics.http4s.Socials.cognitoAuthConf
+import com.malliina.pics.auth.Social.SocialConf
+import com.malliina.pics.http4s.Socials.{cognitoAuthConf, log}
 import com.malliina.util.AppLogger
 import com.malliina.web.*
 import com.malliina.web.IdentityProvider.LoginWithAmazon
-import Social.SocialConf
 
 import java.time.Instant
 
@@ -21,22 +20,23 @@ object Socials:
       override def http = httpClient
 
 class Socials[F[_]: Sync](conf: SocialConf, http: HttpClient[F]):
-  def cognitoValidator(identityProvider: IdentityProvider): CognitoAuthFlow[F] = CognitoAuthFlow(
-    "pics.auth.eu-west-1.amazoncognito.com",
-    identityProvider,
-    Validators.picsId,
-    cognitoAuthConf(conf.amazonConf, http)
-  )
+  private def cognitoValidator(identityProvider: IdentityProvider): CognitoAuthFlow[F] =
+    CognitoAuthFlow(
+      "pics.auth.eu-west-1.amazoncognito.com",
+      identityProvider,
+      Validators.picsId,
+      cognitoAuthConf(conf.amazonConf, http)
+    )
   val twitter = TwitterAuthFlow(conf.twitterConf, http)
   val google = GoogleAuthFlow(conf.googleConf, http)
   val microsoft = MicrosoftAuthFlow(conf.microsoftConf, http)
   val github = GitHubAuthFlow(conf.githubConf, http)
   val amazon = cognitoValidator(LoginWithAmazon)
   val facebook = FacebookAuthFlow(conf.facebookConf, http)
-  val appleToken =
+  private val appleToken =
     if conf.apple.enabled then SignInWithApple(conf.apple).signInWithAppleToken(Instant.now())
     else
       log.info("Sign in with Apple is disabled.")
       ClientSecret("disabled")
-  val appleAuthConf = AuthConf(conf.apple.clientId, appleToken)
+  private val appleAuthConf = AuthConf(conf.apple.clientId, appleToken)
   val apple = AppleAuthFlow(appleAuthConf, AppleTokenValidator(Seq(conf.apple.clientId)), http)
